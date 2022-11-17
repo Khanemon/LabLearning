250aad47c55810f610abd10ec8349b14e5eb0bae
Khanemon <98173245+Khanemon@users.noreply.github.com>
2022-11-17 15:18:27 +0600

    Add files via upload

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,18 @@
 import React from "react";
+import productImage from "./Assets/images/mac.jpg";
+import "./Card.css";
 export const Card =()=>{
-    return<> <div className="root">I am a Card component</div>
-    </>;
+    return<>
+    <div className="root">
+        <div className="image_div">
+            <img className="image" src={productImage} alt=""/>
+        </div> 
+            <div className="content_div">
+                <div className="name">MacBook</div>
+            <p className="Mac_des">You can see the macbook air 2020 vertion. Whitch is very powerfull labtop. It has Apple M1 selicon chip set.  </p>
+        </div>
+        </div>
+    </>
 
 }
 
